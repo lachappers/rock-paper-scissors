2184fc364740b4fbe01b76cd2a7c881334d1b8c6
lachappers <116145040+lachappers@users.noreply.github.com>
2022-10-30 20:22:27 +0000

    playing with variable vs function and looping

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,4 @@
+
 // create choices = rock, paper or scissors
 const choices = ["Rock", "Paper", "Scissors"];
 // rules = rock > scissors, scissors > paper, paper > rock
@@ -12,10 +13,15 @@ function getComputerChoice(){
     //get random index value
     let randomIndex = Math.floor(Math.random() * choices.length);
     //get random item
-    computerChoice = (choices[randomIndex]).toLowerCase();
-    return computerChoice;
+    return computerChoice = (choices[randomIndex]).toLowerCase();
 }
 console.log(getComputerChoice());
+
+
+
+const computerChoice1 = "Computer Choice 1: " + getComputerChoice();
+const computerChoice2 = "Computer Choice 2: " + getComputerChoice();
+console.log(computerChoice1, computerChoice2)
 
 // simultaneously ask player for choice
 let playerSelection = "scissoRs";
@@ -25,8 +31,20 @@ let playerScore = 0;
 console.log(playerSelection);
     // create variable playerSelection(), string, initial value ""
         // ask the user "Rock, paper or scissors?", validate response and store in playerSelection()
-
+// welcome message
+let welcomeMessage = "Welcome to the game"
+    console.log(welcomeMessage);
 // create function playRound()
+function playRound(playerSelection,getComputerChoice){
+   if (playerSelection === getComputerChoice()) {
+        result = "It's a draw!";
+    }
+    else {
+       result = "game on!" 
+    } 
+   return result; 
+}
+console.log(playRound(playerSelection,getComputerChoice));
 // compare choices, return result to playerScore(), display message
  
 

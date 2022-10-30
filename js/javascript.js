153eddf9814b5668a49d8043fd43aaa64e143a5d
lachappers <116145040+lachappers@users.noreply.github.com>
2022-10-30 22:01:18 +0000

    made it work! Need to add player input

--- js/javascript.js
+++ js/javascript.js
@@ -7,15 +7,12 @@ let computerScore = 0;
 let playerScore = 0;
 
 // welcome message
-let welcomeMessage = "Welcome to the game! Round 1:"
+let welcomeMessage = "Welcome to the game!"
     console.log(welcomeMessage);
 
 // ask player for choice
 let playerSelection = "scissoRs";
     playerSelection = playerSelection.toLowerCase();
-// print player choice in console
-console.log(`Your choice: ${playerSelection.toUpperCase()}`);
-
 
 // generate computer choice
     //create function getComputerChoice() which returns randomly assigned value from choices list
@@ -29,6 +26,8 @@ function getComputerChoice(){
 
 // create function playRound()
 function playRound(playerSelection){
+    // print player choice in console
+console.log(`Your choice: ${playerSelection.toUpperCase()}`);
 // set computer choice for this round    
     let computerSelection = getComputerChoice();
 // send message to console   
@@ -36,31 +35,43 @@ function playRound(playerSelection){
 // play round       
    if (playerSelection === computerSelection) {
         result = "It's a draw!";
+        winner = "draw";
     }
     // rules = rock > scissors, scissors > paper, paper > rock
     else if ( (playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "rock" && computerSelection == "scissors")) {
         result = playerSelection.toUpperCase() + " beats " + computerSelection.toUpperCase() + "! You win!"
+        winner = "player";
     }
     else {
         result = computerSelection.toUpperCase() + " beats " + playerSelection.toUpperCase() + "! You lose!"
-    }
-       
+        winner = "computer";
+    }       
    return result; 
 }
 // print result to console
-console.log(playRound(playerSelection));
+// console.log(playRound(playerSelection));
 
 
 // Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
 
 function game() {
-    //let result = "";
     for (let i = 0; i < 5; i++){
-        //result += "Round " + i + "result: ";
+        // run rounds
         let a = i+1
         result = `Round ${a}: ${playRound(playerSelection)}`;
+        // track score
+            if (winner == "player") {
+                playerScore += 1
+            }
+            else if (winner == "computer"){
+                computerScore +=1
+            }
+        let currentResult = `Player: ${playerScore}: Computer: ${computerScore}`;
         console.log(result);
+        console.log(currentResult);
         }
+    // let finalScore = `Final score: ${currentResult}`
+    // console.log(finalScore);
 }
 
 console.log(game());

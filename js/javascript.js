c5cef5e93632654285dc6b0663062bb99619de59
lachappers <116145040+lachappers@users.noreply.github.com>
2022-11-02 20:47:51 +0000

    added button text generator

--- js/javascript.js
+++ js/javascript.js
@@ -81,78 +81,19 @@ function game(rounds) {
     
     return `Final score: ${endResult}`;
 }
- //   return endResult = currentResult;
 
-//let finalScore = `Final score: ${endResult}`
-// console.log(finalScore);
-
-    console.log(game(2));
+    //console.log(game(2));
 
    // let finalScore = `Final Score: Player: ${scores[0]}, Computer: ${scores[1]}`
    // console.log(finalScore);
 
 
+const btns = document.querySelectorAll('#btn');
+    btns.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            console.log(btn.innerHTML);
+        });
+    });
 
-// ************* Cut content ***********
 
- // create variable playerSelection(), string, initial value ""
-        // ask the user "Rock, paper or scissors?", validate response and store in playerSelection()
-
-
-
-/*
-        // game logic
-
-        
-        //List of moves for choices
-        const choices = ["Rock", "Paper", "Scissors"];
-       
-        
-        // randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play
-       function getComputerChoice(){
-            //get random index value
-            let randomIndex = Math.floor(Math.random() * choices.length);
-            //get random item
-            const computerChoice = choices[randomIndex];
-            return computerChoice;
-       }
-       console.log(getComputerChoice());
-
-       //get player choice
-        let playerSelection = 'Rock';
-       //make player choice all lower case
-        playerSelection = playerSelection.toLowerCase();
-       //play round
-       
-       function playRound(playerSelection,computerChoice){
-            // compare playerSeletion and computerChoice
-            // it's a draw
-            if(playerSelection === computerChoice) {
-                result.textContent = "It's a draw! Play again?";
-            } else
-            // set outcome rules
-                if (playerSelection == 'rock'){
-                        if (computerChoice == 'paper') {
-                                result.textContent = "You lose! Paper beats rock!"
-                        } else {
-                            result.textContent = "You win! How surprising!"
-                        }
-            } else
-                if (playerSelection == 'scissors'){
-                        if (computerChoice == 'rock') {
-                            result.textContent = "You lose! Rock beats scissors!"
-                        } else {
-                            result.textContent = "You win! How surprising!"
-                        }
-            } else
-                if (playerSelection == 'paper'){
-                        if (computerChoice == 'scissors') {
-                            result.textContent = "You lose! Scissors beat paper!"
-                        } else {
-                            result.textContent = "You win! How surprising!"
-                        }                
-            }
-            return result.textContent
-        }
-        console.log(playRound(playerSelection,computerChoice));
-        */
+    //console.log(btns);
